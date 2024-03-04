d0be80cecfdca9dc1dd33c026abeb105e2a2e1e3
Kevan Png <kevanpngyeowwei@gmail.com>
2024-03-04 20:01:03 +0800

    fix aws load balancer controller install

--- install-aws-load-balancer-controller.sh
+++ install-aws-load-balancer-controller.sh
@@ -3,5 +3,4 @@ set -ex
 
 # https://artifacthub.io/packages/helm/aws/aws-load-balancer-controller
 # Install the TargetGroupBinding CRDs:
-sudo k0s kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 helm upgrade -i aws-load-balancer-controller ./aws-load-balancer-controller -n kube-system --set clusterName=k0s
