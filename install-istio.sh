6e04f916d93101a954902cbb91a62b2b67ccde71
kevan <kevanpngyeowwei@gmail.com>
2024-03-10 16:02:30 +0800

    generate istio manifests

--- install-istio.sh
+++ install-istio.sh
@@ -1,32 +1,10 @@
-#
-# curl -L https://istio.io/downloadIstio | sh -
-
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.3 TARGET_ARCH=x86_64 sh -
-
-# bring istio to root space
-sudo cp istio-1.20.3/bin/istioctl /usr/bin
-
-# add istioctl to path
-# export PATH=$PWD/bin:$PATH
-
-
-#./istioctl install --set profile=demo -y
-
-# https://discuss.istio.io/t/how-to-hold-a-application-until-proxy-starts/8346/4
-# https://github.com/kubernetes/kubernetes/issues/107260
-# need to fix istio-proxy readiness probe fail
-#  istioctl install --set profile=demo -y --set meshConfig.defaultConfig.holdApplicationUntilProxyStarts=true
-# https://istio.io/latest/docs/reference/commands/istioctl/#istioctl-install
-# https://istio.io/v1.14/blog/2018/aws-nlb/
-istioctl install -y --filename istio-values.yaml
-
 
 export KUBECONFIG=~/admin.conf
+sudo k0s kubectl apply -f istio-manifests.yaml
+
 # alias kubectl='sudo k0s kubectl'
 # configure istio to be enabled for default namespace
 sudo k0s kubectl label namespace default istio-injection=enabled
-# To uninstall istio
-# ./istioctl uninstall --purge
 
-# understanding dns  https://istio.io/v1.12/blog/2020/dns-proxy/
+
 
--- pull-helm-charts.sh
+++ pull-helm-charts.sh
@@ -11,3 +11,27 @@ helm pull eks/aws-load-balancer-controller --untar --version 1.7.1
 
 #### code server
 git clone https://github.com/coder/code-server
+
+#### ISTIO
+#./istioctl install --set profile=demo -y
+
+# https://discuss.istio.io/t/how-to-hold-a-application-until-proxy-starts/8346/4
+# https://github.com/kubernetes/kubernetes/issues/107260
+# need to fix istio-proxy readiness probe fail
+#  istioctl install --set profile=demo -y --set meshConfig.defaultConfig.holdApplicationUntilProxyStarts=true
+# https://istio.io/latest/docs/reference/commands/istioctl/#istioctl-install
+# https://istio.io/v1.14/blog/2018/aws-nlb/
+#istioctl install -y --filename istio-values.yaml
+
+#istioctl manifest generate --filename istio-values.yaml
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.20.3 TARGET_ARCH=x86_64 sh -
+
+# bring istio to root space
+sudo cp istio-1.20.3/bin/istioctl /usr/bin
+
+istioctl manifest generate --filename istio-values.yaml
+# To uninstall istio
+# ./istioctl uninstall --purge
+# understanding dns  https://istio.io/v1.12/blog/2020/dns-proxy/
+
+
